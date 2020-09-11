4ba09a280eb32c3d5e0a90dddc94c45a56bc4856
yo3mil <info@emilwojcik.com>
2020-09-11 12:30:16 +0100

    masonry

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import {VueMasonryPlugin} from 'vue-masonry';
 
+Vue.use(VueMasonryPlugin);
 Vue.config.productionTip = false
 
 new Vue({
